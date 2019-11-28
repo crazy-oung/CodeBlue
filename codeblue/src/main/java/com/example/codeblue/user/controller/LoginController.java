1682a5c7f798a42ec319a5714ae514a0d976f439
crazy-oung <d_yng@naver.com>
2019-11-28 14:12:05 +0900

    부스트랩 적용, 홈페이지 템플릿 만드는 중

---
+++ codeblue/src/main/java/com/example/codeblue/user/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.example.codeblue.user.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/")
+	public String getHome(HttpSession session) {
+		System.out.println("::: get - codeBlue home :::");
+		if(session.getAttribute("user") == null) {
+			return "home";
+		}
+		
+		return "/codeBlue/home";
+	}
+	
+	@GetMapping("/login")
+	public String getLogin(HttpSession session) {
+		System.out.println("::: get - login :::");
+		if(session.getAttribute("user") != null) {
+			return "/codeBlue/home";
+		}
+		
+		return "login";
+	}
+	@GetMapping("/register")
+	public String getRegister(HttpSession session) {
+		System.out.println("::: get - register :::");
+		if(session.getAttribute("user") != null) {
+			return "/codeBlue/home";
+		}
+		
+		return "register";
+	}
+	
+}
